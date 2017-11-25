8e75daa4787288af74bad72d472d6f5c8e6b1d81
qiuyi <qiuyi.hua@uwaterloo.ca>
2017-11-25 14:21:33 -0500

    Implemented expenses page and navigation

---
+++ project/app/src/main/java/alphahacks/senseofexpense/expenses.java
@@ -0,0 +1,13 @@
+package alphahacks.senseofexpense;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class expenses extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expenses);
+    }
+}
